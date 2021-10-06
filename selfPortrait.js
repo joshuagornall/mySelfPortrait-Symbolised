1bda9da371f7f49f228641560683663cffa9a99b
Joshua Gornall <joshua.gornall@yahoo.co.uk>
2021-10-06 10:43:35 +0100

    Create selfPortrait.js

---
+++ selfPortrait.js
@@ -0,0 +1,41 @@
+
+    <h3>Photo</h3>
+
+    <pre id="photo">
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdyssoooyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy/..`  ``` ``..--/+/+yydNMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMNy-             `      `....+mMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMNs.`                    `` `...`/hNMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMN+`                               `-dMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMN:                           `     ``-dMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMN-                              `  ``.:/dMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMdo`                                   `.-/-hMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMNo`                                    .:/:/.:hMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMN/                                   <b>`.../sms.:o</b>NMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMo`                              <b>``..-//:yNMMm::s</b>hMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMy.                  <b>``..---.``.::///++oyNMMMMM+:so</b>MMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMN-                 <b>``..---::::/+ooosydmNMMMMMMN+//+</b>NMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMy-              <b> `....---:://+oyhdmNNNNMMMMMMmy/-+</b>NMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMmo            <b> `....----::/++ohmmNNNNMMMMMMMMN/./</b>mMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMN/          <b>`.....``````..-::/osyhddhhddddmNd..</b>+NMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMy`         <b>`.--...````````  ``-:/+++/:.``./hs`</b>oNMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNo+:.`      <b>.---..```    `.`````.:+o:.`  `:+mN</b>NMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMm:`  <b>```----...``````:-``.``-+mN+..``:NNMM</b>MMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMmh-..<b>`..`-::--........---....-oNMNdyyhmMMM</b>MMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMho:.   <b>`-..``-:----..--::::::-...-+NMMMMMMMMMM</b>MMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMNo.         <b>` `.-------:://///:-....-/hMMMMMMMMMh</b>`oMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMN-              <b>.-:------:::::--.`..--/sMMMh+yNMMh</b>  sMMMMMMMMMMMMMMM
+MMMMMMMMMMMMo               <b>`.-----..........`````.-+dNm+-/dN-</b>  +MMMMMMMMMMMMMMM
+MMMMMMMMMMMM.               <b>`.--...````````````````.oNMMo:.o/</b>   +MMMMMMMMMMMMMMM
+MMMMMMMMMMMM/                <b>`....`````    ````.-:/+hmmy-:/y</b>    oMMMMMMMMMMMMMMM
+MMMMMMMMMMMMs                <b>``..`````````     ```.-+shyydh.</b>    dMMMMMMMMMMMMMMM
+MMMMMMMMMMMMy                 <b>``````````..``````--:/yMMNmh.</b>     /mMMMMMMMMMMMMMM
+MMMMMMMMMMMMs                    <b>`````` ``````````.-oNNh+.</b>       .+oyhhmMMMMMMMM
+MMMMMMMMMMMM.                  <b>`    ````````....-:ohmNd/`</b>                 `-:+sh
+MMMMMMMMMMMs                                <b>`...--/ydy:</b>
+MMMMMMMMmdy-                                      <b>`.-.</b>
+MMMNy+-.
+ms:                                          <b>```</b>
+
+    </pre>
